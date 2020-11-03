ca653abf281858cc830d1130523afd52970a6c78
Andrei Hruzdzeu <andrei.hruzdzeu@rallyhealth.com>
2020-11-03 09:27:07 -0800

    Implement few more state-related methods

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -2,7 +2,6 @@ package fpinscala.state
 
 import scala.annotation.tailrec
 
-
 trait RNG {
   def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
 }
@@ -12,10 +11,18 @@ object RNG {
 
   case class Simple(seed: Long) extends RNG {
     def nextInt: (Int, RNG) = {
-      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
-      val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
-      val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
-      (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
+      val newSeed =
+        (seed * 0x5deece66dL + 0xbL) & 0xffffffffffffL // `&` is bitwise AND. We use the current seed to generate a new seed.
+      val nextRNG =
+        Simple(
+          newSeed
+        ) // The next state, which is an `RNG` instance created from the new seed.
+      val n =
+        (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
+      (
+        n,
+        nextRNG
+      ) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
     }
   }
 
@@ -26,16 +33,17 @@ object RNG {
   def unit[A](a: A): Rand[A] =
     rng => (a, rng)
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
     rng => {
       val (a, rng2) = s(rng)
       (f(a), rng2)
     }
 
-  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
       val (a, r) = f(rng)
       g(a)(r)
-  }
+    }
 
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i, r) = rng.nextInt
@@ -47,7 +55,8 @@ object RNG {
     (i / (Int.MaxValue.toDouble + 1), r)
   }
 
-  def doubleMap: Rand[Double] = map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
+  def doubleMap: Rand[Double] =
+    map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
 
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
     rnd => {
@@ -56,36 +65,35 @@ object RNG {
       (f(a, b), br)
     }
 
-  def _map[A,B](s: Rand[A])(f: A => B): Rand[B] = {
+  def _map[A, B](s: Rand[A])(f: A => B): Rand[B] = {
     flatMap(s)(a => unit(f(a)))
   }
 
-
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
     @tailrec
-    def sequence(rng: RNG, fs: List[Rand[A]], as: List[A]): (List[A], RNG) = fs match {
-      case List() => (as, rng)
-      case f::fs =>
-        val (a, ar) = f(rng)
-        sequence(ar, fs, a::as)
-    }
+    def sequence(rng: RNG, fs: List[Rand[A]], as: List[A]): (List[A], RNG) =
+      fs match {
+        case List() => (as, rng)
+        case f :: fs =>
+          val (a, ar) = f(rng)
+          sequence(ar, fs, a :: as)
+      }
     rnd => sequence(rnd, fs, List[A]())
   }
 
-
-  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
     val (i, r1) = nonNegativeInt(rng)
     val (d, r2) = double(r1)
     ((i, d), r2)
   }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
     val (d, r1) = double(rng)
     val (i, r2) = nonNegativeInt(r1)
     ((d, i), r2)
   }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
     val (d1, r1) = double(rng)
     val (d2, r2) = double(r1)
     val (d3, r3) = double(r2)
@@ -98,26 +106,44 @@ object RNG {
       if (c == 0) (is, rng)
       else {
         val (i, r) = rng.nextInt
-        ints(c - 1, i::is)(r)
+        ints(c - 1, i :: is)(r)
       }
 
     ints(count, Nil)(rng)
   }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }
 
-case class State[S,+A](run: S => (A, S)) {
-  def map[B](f: A => B): State[S, B] =
-    ???
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    ???
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    ???
+case class State[S, +A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] = {
+    val r = (s: S) => {
+      val (a, sa) = this.run(s)
+      (f(a), sa)
+    }
+    State(r)
+  }
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = {
+    val rn = (s: S) => {
+      val (a, sa) = this.run(s)
+      val (b, sb) = sb(sa)
+      (f(a, b), sb)
+    }
+    State(rn)
+  }
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = {
+    val rn = (s: S) => {
+      val (a, sa) = this.run(s)
+      f(a).run(sa)
+    }
+    State(rn)
+  }
 }
 
 sealed trait Input
