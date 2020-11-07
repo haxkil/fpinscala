f5b635f71d111fbf770c3be40d7e159579e60ec5
Andrei Hruzdzeu <andrei.hruzdzeu@rallyhealth.com>
2020-11-06 16:47:40 -0800

    Implement few more parallel-related methods

---
+++ exercises/src/main/scala/fpinscala/parallelism/ParTest.scala
@@ -0,0 +1,22 @@
+package fpinscala.parallelism
+
+import org.scalatest.funspec.AnyFunSpec
+
+class ParTest extends AnyFunSpec {
+
+  describe("ParTest") {
+    it("should fork") {}
+
+    it("should run") {}
+
+    it("should sortPar") {}
+
+    it("should delay") {}
+
+    it("should map") {}
+
+    it("should map2") {}
+
+    it("should unit") {}
+  }
+}
